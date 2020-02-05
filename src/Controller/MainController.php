d9bc8a9c928d42986bbe259f2ca50a8e99caa3c1
camaelfr <37018854+camaelfr@users.noreply.github.com>
2020-02-05 21:16:08 +0100

    shaping registerForm + adding content
    Basic shaping of register form, not finished.
    Adding content and link for presentation page

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -16,4 +16,24 @@ class MainController extends Controller
             'controller_name' => 'MainController',
         ]);
     }
+
+    /**
+     * @Route("/contact", name="contact")
+     */
+    public function contact()
+    {
+        return $this->render('main/contact.html.twig', [
+            'controller_name' => 'MainController',
+        ]);
+    }
+
+    /**
+     * @Route("/presentation", name="presentation")
+     */
+    public function presentation()
+    {
+        return $this->render('main/presentation.html.twig', [
+            'controller_name' => 'MainController',
+        ]);
+    }
 }
